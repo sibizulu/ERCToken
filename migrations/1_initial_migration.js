87a14d3b64de4f8f2a834b5cca06ac3146fca2f5
sibizulu <sibiraj@lightrains.com>
2018-06-15 07:50:15 +0530

    Remove uplock

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,14 +1,5 @@
 var Migrations = artifacts.require('./Migrations.sol');
-var Web3 = require('web3');
-
-var web3 = new Web3(
-  new Web3.providers.HttpProvider('http://192.168.27.101:8545')
-);
 
 module.exports = function(deployer) {
-  web3.personal
-    .unlockAccount('0xd4cefe4bc33681c9eaf5c0ecbca2f35c12a20561', '123', 15000)
-    .then(() => {
-      deployer.deploy(Migrations);
-    });
+  deployer.deploy(Migrations);
 };
