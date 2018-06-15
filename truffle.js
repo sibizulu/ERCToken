914c925b707e8fe7189aab91cc0a973d17ffe703
Light Server <nikhil@lightrains.com>
2018-06-15 10:01:30 +0530

    Adding

--- truffle.js
+++ truffle.js
@@ -4,8 +4,7 @@ module.exports = {
       host: '192.168.27.101',
       port: 8545,
       gas: 4700000,
-      network_id: '3',
-      from: "0xD4CEFe4BC33681c9eAf5C0eCbCA2F35c12A20561"
+      network_id: '3'
     }
   }
 };
